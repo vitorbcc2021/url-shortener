1792bcfb23f0e62397f54dfed3a93861edcff893
Vitor Allace <vitorallacepnn@gmail.com>
2025-08-29 14:53:16 -0300

    fix: bcrypt.compare is now being used correctly

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -7,11 +7,13 @@ export async function login(req, res) {
     try {
         const dto = await AuthDto.create(req.body)
 
-        const user = await UserModel.findOne({ username: req.body.username })
+        const user = await UserModel.findOne({ username: dto.username })
 
-        if (!user) {
-            return res.status(404).json({ error: 'User not found!' })
-        } else if (bcrypt.compare(req.body.password, user.password)) {
+        if (!user) return res.status(404).json({ error: 'User not found!' })
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+
+        if (isPasswordCorrect) {
             const payload = { username: user.username, role: user.role }
             const SECRET = process.env.SECRET
             const token = jwt.sign(payload, SECRET, { expiresIn: '2h' })
@@ -27,7 +29,7 @@ export async function login(req, res) {
     }
 }
 
-export async function loginAsRecruiter(req, res) {
+export async function loginAsRecruiter(res) {
     try {
         const user = await UserModel.findOne({ username: 'Recruiter-Profile' })
 
