b884e699255fcbd5d874f8551016bd0121578679
Vitor Allace <vitorallacepnn@gmail.com>
2025-09-22 15:06:33 -0300

    fix: now the app is listening

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ connectDB()
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
-
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
